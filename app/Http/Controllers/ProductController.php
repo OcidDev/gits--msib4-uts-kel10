0807228218fdef36a4741e2b1a88ba28c831f5fe
ifanhananwijianto <ifanhanan22@gmail.com>
2023-04-11 14:49:25 +0700

    add fitur product

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products =product::with('category')->get();
+        return view('pages/product.index')->with('products',$products);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories['categories'] = Category::all();
+        return view('pages/product.create', $categories );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+         Product::create([
+            'name'=>$request->input('name'),
+            'description'=>$request->input('description'),
+            'price'=>$request->input('price'),
+            'categories_id'=>$request->input('category_id'),
+        ]);
+
+        // $input = $request->all();
+        // Product::create($input);
+        return redirect('product')->with('flash_message', 'Product created!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data['categories'] = Category::all();
+        $data['products'] = Product::find($id);
+
+        return view('pages/product.edit', $data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $input = $request->all();
+        $product->update($input);
+        return redirect('product')->with('flash_message', 'Product updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        product::destroy($id);
+         return redirect('/product');
+    }
+}
---
+++ resources/views/pages/product/create.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.dashboard')
+@section('title')
+    Add Product
+@endsection
+
+@section('content')
+    <div class="card" style="margin: 20px;">
+        <div class="card-header"><h3>Create New Product</h3></div>
+            <div class="card-body">
+                <form action="{{ url('/product') }}" method="POST">
+                    @csrf
+                    <label>Name</label><br>
+                        <input type="text" name="name" id="name" class="form-control"><br>
+                    <label>Price</label><br>
+                        <input type="text" name="price" id="price" class="form-control"><br>
+                    <label>Description</label><br>
+                        <input type="text" name="description" id="description" class="form-control"><br>
+                        <br>
+                        <select class="from-select" aria-label="Default select example" name="category_id"  id="category_id">
+                            <option selected>Pilih Categori product</option>
+                            @foreach ($categories as $item)
+                                <option value="{{ $item->id }}">{{ $item->name }}</option>
+
+                            @endforeach
+                        </select><br><br></br>
+                        <input type="submit" value="Save" class="btn btn-success"><br>
+                </form>
+            </div>
+    </div>
+
+
+@endsection
+
